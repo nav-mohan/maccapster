35811a9a9bcf3362779a5858169e81ccdfbdbf7f
nav-mohan <navaneeth.mohan94@gmail.com>
2023-10-25 19:49:03 -0400

    start using ms_logger in main.cpp

--- main.mm
+++ main.mm
@@ -11,6 +11,8 @@
 #include "helpers.mm"
 #include "state.h"
 
+#include "mslogger.hpp"
+
 #include "gui.h"
 
 boost::asio::io_context ioContext;
@@ -19,29 +21,39 @@ Client clientConn2(ioContext);
 XmlHandler xmlHandler;
 RS232Util relayControl;
 GeoUtil geoUtil;
+UserSettings settings;
 
 // std::ofstream capturedOutput("output.txt");
 int main(int argc, char *argv[])
 {
-    std::cout << "Press <RETURN> to start" << std::endl;
-    getchar();
+    // std::cout << "Press <RETURN> to start" << std::endl;
+    // getchar();
+    MsLogger<DEBUG>::get_instance().log_to_stdout("CONFIGURATION");
+    MsLogger<INFO>::get_instance().log_to_file("CONFIGURATION",DEBUG);
 
-    UserSettings settings;
+    MsLogger<DEBUG>::get_instance().log_to_stdout("SERVER1 " + settings.server1_ + ":" + settings.port1_);
+    MsLogger<INFO>::get_instance().log_to_file("SERVER1 " + settings.server1_ + ":" + settings.port1_,DEBUG);
 
-    std::cout << "CONFIGURATION" << std::endl;
-    std::cout << "SERVER1 " << settings.server1_ << ":" << settings.port1_ << std::endl;
-    std::cout << "SERVER2 " << settings.server2_ << ":" << settings.port2_ << std::endl;
-    std::cout << "COORDINATES " << settings.latitude_ << "," << settings.longitude_ << std::endl;
+    MsLogger<DEBUG>::get_instance().log_to_stdout("SERVER2 " + settings.server2_ + ":" + settings.port2_);
+    MsLogger<INFO>::get_instance().log_to_file("SERVER2 " + settings.server2_ + ":" + settings.port2_,DEBUG);
+
+    MsLogger<DEBUG>::get_instance().log_to_stdout("COORDINATES " + settings.latitude_ + "," + settings.longitude_);
+    MsLogger<INFO>::get_instance().log_to_file("COORDINATES " + settings.latitude_ + "," + settings.longitude_,DEBUG);
+
 
     clientConn1.set_endpoint(settings.server1_,settings.port1_);
     clientConn1.setTimerFrequency(atof(settings.heartbeatFreq_.c_str()));
-    std::cout << "SET ENDPOINT 1" << std::endl;
+    MsLogger<DEBUG>::get_instance().log_to_stdout("SET ENDPOINT 1");
+    MsLogger<INFO>::get_instance().log_to_file("SET ENDPOINT 1",DEBUG);
+
     clientConn2.set_endpoint(settings.server2_,settings.port2_);
     clientConn2.setTimerFrequency(atof(settings.heartbeatFreq_.c_str()));
-    std::cout << "SET ENDPOINT 2" << std::endl;
+    MsLogger<DEBUG>::get_instance().log_to_stdout("SET ENDPOINT 2");
+    MsLogger<INFO>::get_instance().log_to_file("SET ENDPOINT 2",DEBUG);
 
     geoUtil.set_station(settings.latitude_,settings.longitude_);
-    std::cout << "SET LAT LON" << std::endl;
+    MsLogger<DEBUG>::get_instance().log_to_stdout("SET LAT/LON");
+    MsLogger<INFO>::get_instance().log_to_file("SET LAT/LON ",DEBUG);
 
     PBQueue playbackQueue;
     PBUtil playbackUtil;
@@ -55,10 +67,9 @@ int main(int argc, char *argv[])
     playbackQueue.Play = [&](const std::string f){
         playbackUtil.OpenFile(f);
     };
-    std::cout << "CONFIGURED PLAYBACK QUEUE" << std::endl;
+    MsLogger<DEBUG>::get_instance().log_to_stdout("CONFIGURED PLAYBACKQUEUE");
+    MsLogger<INFO>::get_instance().log_to_file("CONFIGURED PLAYBACKQUEUE",DEBUG);
 
-
-    std::cout << "CONFIGURING DOWNLOAD QUEUE" << std::endl;
     std::vector<std::string>certfiles = {getResourcePath("cacert.pem")};
     std::string certbuffer;
     std::string buffer;
@@ -71,42 +82,48 @@ int main(int argc, char *argv[])
         fclose(certfile);
     }
     auto lambda = [&](std::string filename){
-        printf("DW DONE %s\n",filename.c_str());
+        MsLogger<INFO>::get_instance().log_to_stdout("DOWNLOAD COMPLETED " + filename);
+        MsLogger<INFO>::get_instance().log_to_file("DOWNLOAD COMPLETED " + filename);
         playbackQueue.Push(filename);
     };
     DWQueue downloadQueue(ioContext,std::move(lambda),std::move(certbuffer));
-    std::cout << "CONFIGURED DOWNLOAD QUEUE" << std::endl;
+    MsLogger<DEBUG>::get_instance().log_to_stdout("CONFIGURED DOWNLOADQUEUE");
+    MsLogger<INFO>::get_instance().log_to_file("CONFIGURED DOWNLOADQUEUE",DEBUG);
 
     xmlHandler.decodeToFile = [&](std::string&& encodedData, const std::string& filename){
         decode_and_write(std::move(encodedData),filename);
-        printf("DEC DONE %s\n",filename.c_str());
+        MsLogger<INFO>::get_instance().log_to_stdout("DECODING COMPLETED " + filename);
+        MsLogger<INFO>::get_instance().log_to_file("DECODING COMPLETED " + filename);
         playbackQueue.Push(filename);
     };
-    std::cout << "CONFIGURED XMLHANDLER DECODER" << std::endl;
-
-    std::string storageFolder = "/Users/nav/Downloads/";
-    std::atomic<std::string*> ptrStorageFolder;
+    MsLogger<DEBUG>::get_instance().log_to_stdout("CONFIGURED XMLHANDLER DECODER");
+    MsLogger<INFO>::get_instance().log_to_file("CONFIGURED XMLHANDLER DECODER",DEBUG);
+    
     xmlHandler.enqueueDwnld = [&](const std::string& uri, const std::string& filename){
         Downloadable d;
         d.filename_ = filename;
         ParseURL(uri,d.host_,d.target_,d.port_);
         downloadQueue.Push(d);
     };
-    std::cout << "CONFIGURED XMLHANDLER ENQUEUDW" << std::endl;
-    
+    MsLogger<DEBUG>::get_instance().log_to_stdout("CONFIGURED XMLHANDLER ENQUEUDW");
+    MsLogger<INFO>::get_instance().log_to_file("CONFIGURED XMLHANDLER ENQUEUDW",DEBUG);
+
     xmlHandler.checkArea = [&](std::string &&boundary){
         geoUtil.set_alertArea(std::move(boundary));
         return geoUtil.is_inside();
     };
-    std::cout << "CONFIGURED XMLHANDLER CHECKAREA" << std::endl;
+    MsLogger<DEBUG>::get_instance().log_to_stdout("CONFIGURED XMLHANDLER CHECKAREA");
+    MsLogger<INFO>::get_instance().log_to_file("CONFIGURED XMLHANDLER CHECKAREA",DEBUG);
 
     TextToSpeechQueue *ttsq = [[TextToSpeechQueue alloc]init];
     PBQueue *pbq = &playbackQueue;
     ttsq->onComplete = ^(NSString* filename){
-        printf("TTS JOB DONE\n");
+        MsLogger<INFO>::get_instance().log_to_stdout("TTS JOB DONE " + std::string(filename.UTF8String));
+        MsLogger<INFO>::get_instance().log_to_file("TTS JOB DONE " + std::string(filename.UTF8String));
         pbq->Push([filename UTF8String]);
     };
-    std::cout << "CONFIGURED TTSQUEUE" << std::endl;
+    MsLogger<DEBUG>::get_instance().log_to_stdout("CONFIGURED XMLHANDLER TTSQUEUE");
+    MsLogger<INFO>::get_instance().log_to_file("CONFIGURED XMLHANDLER TTSQUEUE",DEBUG);
 
     xmlHandler.enqueueTTS = [&](std::string&& ttsText, std::string& filename, const std::string& language){
         std::string output = std::move(prepareTextForTTS(std::move(ttsText)));
@@ -125,33 +142,37 @@ int main(int argc, char *argv[])
         [ttsq enqueueText : sp];
         [sp release];
     };
-    std::cout << "CONFIGURED XMLHANDLER ENQUEUTTS" << std::endl;
+    MsLogger<DEBUG>::get_instance().log_to_stdout("CONFIGURED XMLHANDLER ENQUEUETTS");
+    MsLogger<INFO>::get_instance().log_to_file("CONFIGURED XMLHANDLER ENQUEUETTS",DEBUG);
 
     clientConn1.appendXml = [&](std::string& host, const std::vector<char>&& tempBuffer, size_t size){
         xmlHandler.append(host,std::move(tempBuffer),size);
     };
-    std::cout << "CONFIGURED CLIENTCONN1 APPENDXML" << std::endl;
+    MsLogger<DEBUG>::get_instance().log_to_stdout("CONFIGURED CLIENTCONN1 APPENDXML");
+    MsLogger<INFO>::get_instance().log_to_file("CONFIGURED CLIENTCONN1 APPENDXML",DEBUG);
     
     clientConn2.appendXml = [&](std::string& host, const std::vector<char>&& tempBuffer, size_t size){
         xmlHandler.append(host,std::move(tempBuffer),size);
     };
-    std::cout << "CONFIGURED CLIENTCONN2 APPENDXML" << std::endl;
+    MsLogger<DEBUG>::get_instance().log_to_stdout("CONFIGURED CLIENTCONN2 APPENDXML");
+    MsLogger<INFO>::get_instance().log_to_file("CONFIGURED CLIENTCONN2 APPENDXML",DEBUG);
 
     clientConn1.start();
-    std::cout << "STARTED CLIENTCONN1" << std::endl;
+    MsLogger<INFO>::get_instance().log_to_stdout("STARTED CLIENTCONN1");
+    MsLogger<INFO>::get_instance().log_to_file("STARTED CLIENTCONN1");
     // clientConn2.start();
     std::thread t1([&](){
         ioContext.run();
     });    
     // Create the application instance
-    NSApplication *application = [NSApplication sharedApplication];
+    // NSApplication *application = [NSApplication sharedApplication];
     
     // Create the app delegate
-    AppDelegate *delegate = [[AppDelegate alloc] init];
+    // AppDelegate *delegate = [[AppDelegate alloc] init];
     
     // Set the delegate for the application
-    [application setDelegate:delegate];
-    [application run];
+    // [application setDelegate:delegate];
+    // [application run];
     [[NSRunLoop mainRunLoop]run];
     t1.join();
     return 0;
