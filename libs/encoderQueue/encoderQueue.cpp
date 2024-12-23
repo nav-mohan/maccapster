685daada1787eab3be8b1b65d52c9d6999c4b9c6
nav-mohan <navaneeth.mohan94@gmail.com>
2024-12-22 20:46:44 -0500

    ISSUE#2 : implement EncoderQueue

---
+++ libs/encoderQueue/encoderQueue.cpp
@@ -0,0 +1,40 @@
+#include "encoderQueue.hpp"
+
+void EncQueue::Handler()
+{
+    std::unique_lock<std::mutex> lock(mut_);
+
+    do
+    {
+        cond_.wait(lock, [this](){
+            return (busy_.load() == 0 && (queue_.size()||quit_.load()));
+        });
+        if(queue_.size() && quit_.load()==0 && busy_.load()==0)
+        {
+            std::string filename = std::move(queue_.front());
+            queue_.pop();
+            busy_.store(1);
+            lock.unlock();
+            Encode(filename); // this blocks the workerThread_
+            if(queue_.size()==0) OnFinish();
+            lock.lock();
+            busy_.store(0);
+        }
+    } while (quit_.load() == 0);
+}
+
+void EncQueue::Push(const std::string& f)
+{
+    std::lock_guard<std::mutex> lock(mut_);
+    queue_.push(f);
+    cond_.notify_one();
+}
+
+EncQueue::~EncQueue()
+{
+    std::unique_lock<std::mutex> lock(mut_);
+    quit_.store(1);
+    cond_.notify_one();
+    lock.unlock();
+    if(workerThread_.joinable()) workerThread_.join();
+}
---
+++ libs/encoderQueue/encoderQueue.hpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <queue>
+#include <mutex>
+#include <thread>
+#include <condition_variable>
+#include <atomic>
+#include <functional>
+
+// EncQueue has it's own dedicated thread
+// EncQueue is triggered by TTSQueue
+// we will create a separate ZipQueue which is triggered once a day
+// EncQueue only handles WAV files
+// whereas the ZipQueue will handle MP3 and XML files
+// EncQueue will also handle deletion of oriignal WAV files based on return code of encoder
+// ZipQueue will handle creation of new folder, moving 1-day-old files to new-folder, zipping, deleting new-folder, and moving zipped tarball into another folder
+struct EncQueue
+{
+    std::queue<std::string> queue_; // a queue of WAV filepaths 
+    std::mutex mut_;
+    std::thread workerThread_;
+    std::condition_variable cond_;
+    void Handler();
+    void Push(const std::string& filename);
+    void Push(const char * filename){Push(std::string(filename));}
+    EncQueue() : workerThread_(&EncQueue::Handler, this){};
+    ~EncQueue();
+    std::atomic_bool quit_ = ATOMIC_VAR_INIT(0);
+    std::atomic_bool busy_ = ATOMIC_VAR_INIT(0);
+
+    std::function<void(const std::string& filename)> Encode;
+    std::function<void()> OnFinish; // upon emptying the queue, delete the original WAV file
+};
