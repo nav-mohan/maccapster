d1d367e955234a7324d4b7ce16dca02e54208086
nav-mohan <navaneeth.mohan94@gmail.com>
2024-12-25 17:29:18 -0500

    ISSUE #2: Implement MONO AAC-Encoder
    The code is a cludge because I dont fully understand the parameter specification for FDK-AAC.
    There is an upper-limit to the buffer-size. I think it's 2048 samples for STEREO and 1024 samples for MONO.
    AACEndoder::DoEncodeMono and AACEncoder::DoEncodeInterleaved are pretty much the same except for this upper-limit

--- libs/encoderQueue/aacencoder.hpp
+++ libs/encoderQueue/aacencoder.hpp
@@ -42,8 +42,11 @@ struct AACEncoder
 	    aacEncOpen(&handle, 0, m_channels);
 	    aacEncoder_SetParam(handle, AACENC_AOT, 2);
 	    aacEncoder_SetParam(handle, AACENC_SAMPLERATE, m_samplerate);
-	    aacEncoder_SetParam(handle, AACENC_CHANNELMODE, MODE_2);
-	    aacEncoder_SetParam(handle, AACENC_CHANNELORDER, 1);
+        if(m_channels == 1)
+	        aacEncoder_SetParam(handle, AACENC_CHANNELMODE, MODE_1); // mono
+        else 
+	        aacEncoder_SetParam(handle, AACENC_CHANNELMODE, MODE_2); // stereo
+	    // aacEncoder_SetParam(handle, AACENC_CHANNELORDER, 2); // commented it out -- AACENC_CHANNELORDER will be set to default as per number-of-channels
         aacEncoder_SetParam(handle, AACENC_BITRATE, m_bitrate);
 	    aacEncEncode(handle, NULL, NULL, NULL, NULL);
 
@@ -71,14 +74,50 @@ struct AACEncoder
         aacEncClose(&handle);
     }	
 
+// ----------------------------------------------------------------------
 
+// DoEncode Mono and DoEncodInterleaved are exactly the same function because I'm not sure how to 
+// set the parameters. Particulalry, I'm confused about the bufferSize. 
+// FDK-AAC is very finicky about the number of bytes it can process per channel. 
+// I think it maxes out at 2048 samples for STEREO and 1024 samples for MONO
+// it might also depend on the value of AACENC_AOT (Audio Object Type) 
+// If you want to play it safe, 2048 bytes (i.e 1024 samples) works for Mono and Stereo
+// Make sure that the client-code (main.mm) is passing in at-most 2048 bytes at a time. 
+
+// ----------------------------------------------------------------------
+
+    constexpr int       DoEncodeMono(const void *pcmBuffer, const uint32_t pcmBufferSize)
+    {
+        memset(m_pcmBufferInternal, 0, 2 * CALCULATE_CHANNEL_BUFFER_SIZE);
+        memcpy(m_pcmBufferInternal, pcmBuffer, pcmBufferSize);
+        int err = 0;
+
+        int bytes_to_be_processed = 2048; 
+
+        m_pcmArgs.numInSamples = bytes_to_be_processed <= 0 ? -1 : bytes_to_be_processed/2;
+        m_pcmBuffDesc.bufSizes = &bytes_to_be_processed;
+        // printf("MEMSETTING AAC %d\n",bytes_to_be_processed);
+        memset(m_pcmBuffer, 0, bytes_to_be_processed);
+        // printf("MEMCOPYING AAC %d\n",bytes_to_be_processed);
+        memcpy(m_pcmBuffer,m_pcmBufferInternal,bytes_to_be_processed);
+        err = aacEncEncode(handle, &m_pcmBuffDesc, &m_aacBuffDesc, &m_pcmArgs, &m_aacArgs);
+        fwrite(m_encBuffer, 1, m_aacArgs.numOutBytes, outfile);
+        // printf("WROTE %d BYTES\n",m_aacArgs.numOutBytes);
+        m_bytesEncoded = m_aacArgs.numOutBytes;
+        return m_bytesEncoded;
+
+    }
+// ----------------------------------------------------------------------
+
+    // FDK-AAC is finicky about pcmBufferSize. It maxes out at 2048 for MONO and 4096 for Stereo
     constexpr int       DoEncodeInterleaved(const void *pcmBuffer, const uint32_t pcmBufferSize)
     {
         memset(m_pcmBufferInternal, 0, 2 * CALCULATE_CHANNEL_BUFFER_SIZE);
         memcpy(m_pcmBufferInternal, pcmBuffer, pcmBufferSize);
         int err = 0;
 
-        int bytes_to_be_processed = 8192; // FDK-AAC can only support 2048 samples per channel according to API documentation
+        int bytes_to_be_processed = 4096; 
+
         m_pcmArgs.numInSamples = bytes_to_be_processed <= 0 ? -1 : bytes_to_be_processed/2;
         m_pcmBuffDesc.bufSizes = &bytes_to_be_processed;
         // printf("MEMSETTING AAC %d\n",bytes_to_be_processed);
